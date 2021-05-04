88896c926270aa2169bf9f627911d12cf31223a6
avinashvidyarthi <avinashvidyarthi@gmail.com>
2021-05-04 09:45:11 +0530

    added routes

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const http = require('http');
+const express = require('express');
 const port = process.env.PORT || 3000;
-http.createServer(function (req, res) {
-  res.write('Hello World Updated!'); 
-  res.end(); 
-}).listen(port); 
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 'ok',
+    message: 'Hello World!'
+  })
+});
+
+app.get('/ping', (req, res) => {
+	res.json({
+		status: 'ok',
+		message: 'pong',
+	});
+});
+
+app.listen(port,()=>{
+  console.log('Server listening on port:', port);
+})
